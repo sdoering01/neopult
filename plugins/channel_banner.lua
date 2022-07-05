8418c95ed16b46153b428450da058599a6459ebd
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2022-07-05 22:42:59 +0200

    Set screen back to channel banner size on cleanup

--- plugins/channel_banner.lua
+++ plugins/channel_banner.lua
@@ -9,7 +9,11 @@ M.setup = function(args)
 
     local resolution = args.resolution or DEFAULT_RESOLUTION
 
-    M.plugin_handle = api.register_plugin_instance("channel-banner")
+    M.plugin_handle = api.register_plugin_instance("channel-banner", { on_cleanup = function()
+        if M.window_handle then
+            M.window_handle:max(resolution)
+        end
+    end })
     if M.plugin_handle then
         M.window_handle = M.plugin_handle:claim_window('zathura', { ignore_managed = true })
         if M.window_handle then
