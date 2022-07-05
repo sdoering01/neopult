185bef9c6e7e90b9c5b4350182c56337f60af348
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2022-07-05 19:39:43 +0200

    Add plugin that manages the channel banner

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- load plugins here
+require("channel_banner").setup({ resolution = { 1920, 1080 } })
 require("cvh_camera").setup({
     camera_server_path = "../cvh-camera/camera-server/dist/server.js",
     sender_base_url = "http://localhost:3000/camera-sender.html",
---
+++ plugins/channel_banner.lua
@@ -0,0 +1,21 @@
+local api = neopult.api
+
+local DEFAULT_RESOLUTION = { 1920, 1080 }
+
+local M = {}
+
+M.setup = function(args)
+    args = args or {}
+
+    local resolution = args.resolution or DEFAULT_RESOLUTION
+
+    M.plugin_handle = api.register_plugin_instance("channel-banner")
+    if M.plugin_handle then
+        M.window_handle = M.plugin_handle:claim_window('zathura', { ignore_managed = true })
+        if M.window_handle then
+            M.window_handle:max(resolution)
+        end
+    end
+end
+
+return M
