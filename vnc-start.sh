80d5aab2b21ee229a17e887678f4e22502f0bb22
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2022-07-05 23:27:53 +0200

    Fix bugs related to the channel banner

--- vnc-start.sh
+++ vnc-start.sh
@@ -10,5 +10,8 @@ fi
 
 Xvnc :5 -auth "$HOME/.Xauthority" -rfbport 5905 -geometry 1920x1080 -depth 24 -pn -localhost -rfbauth "$HOME/.vnc/passwd" -nocursor &
 
+# Wait for vnc server to start
+sleep 1
+
 export DISPLAY=:5
 zathura --mode=presentation --page=1 assets/vnc/channel-banner.pdf
