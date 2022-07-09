e18abcf44dd80284773d9652b1673ccf80b45528
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2022-07-09 20:37:25 +0200

    Change init-example.lua

--- init-example.lua
+++ init-example.lua
@@ -8,8 +8,10 @@ require("cvh_camera").setup({
     -- Change this to match the domain where the camera sender is served
     sender_base_url = "https://your-domain.com/camera-sender.html",
     cameras = 2,
-    -- Has to match the view-only password of the vnc channel
+    -- Has to match the view-only password of the vnc channel and has to be public
     janus_room_pin = "testcvh",
+    -- This has to match the admin key in the general block of janus.plugin.videoroom.jcfg and should REMAIN PRIVATE
+    janus_admin_key = "secret",
 })
 require("vnc").setup({ listen = 2 * channel })
 require("vnc").setup({ listen = 2 * channel + 1 })
